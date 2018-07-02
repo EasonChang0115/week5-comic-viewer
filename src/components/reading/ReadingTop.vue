d11797010a8fb117e29512a01dc04c185416fcc2
KONEX-PC\s8710 <s8710606@gmail.com>
2018-07-02 18:04:11 +0800

    reading css

--- src/components/reading/ReadingTop.vue
+++ src/components/reading/ReadingTop.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="reading-top">
-    ReadingTop
+    <div class="title3">My Hexschool</div>
+
+    <i class="fas fa-caret-right"></i>
+
+    <select class="chepter-select" name="" id="">
+      <option value="">chepter1</option>
+    </select>
+    <select class="page-select" name="" id="">
+      <option value="">page1</option>
+      <option value="">page2</option>
+      <option value="">page3</option>
+    </select>
+
+    <div class="day-and-night">
+      <i class="fas fa-sun"></i>
+      <div class="switch-dark">
+        <label for="switch-dark"></label>
+        <input type="checkbox" name="" id="switch-dark">
+      </div>
+      <i class="far fa-moon"></i>
+    </div>
   </div>
 </template>
 
