d072d450d496ee78b4f2fd71821494c0a53b4a30
KONEX-PC\s8710 <s8710606@gmail.com>
2018-07-03 15:41:00 +0800

    slider-scroll

--- src/components/reading/ReadingSlider.vue
+++ src/components/reading/ReadingSlider.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="reading-slider">
     <div class="slider">
-      <div class="inner-slider">
+      <div class="inner-slider" :style="{left: `${position}px`}">
         <div class="comic-imgs" v-for="(page, index) in localChapterPages" 
           :key="index">
-          <div class="num" :class="nowPage === index ? 'now' : ''">{{ pendingNumber(index+1) }}</div>
+          <div class="num" :class="nowPage === index ? 'hide' : ''">{{ pendingNumber(index+1) }}</div>
           <div class="img" 
           :style="{backgroundImage: `url(${page.url})`}"
-          :class="nowPage === index ? 'now' : ''" 
+          :class="nowPage === index ? 'nowComic' : ''" 
           ><div class="shadow"></div></div>
         </div>
       </div>
     </div>
     <div class="pre-button" @click="changeChapter(-1)">
-        <i class="fas fa-angle-double-left"></i>
-        <div class="text">PREVIOUS CHAPTER</div>
+      <i class="fas fa-angle-double-left"></i>
+      <div class="text">PREVIOUS CHAPTER</div>
+    </div>
+    <div class="next-button" @click="changeChapter(1)">
+      <i class="fas fa-angle-double-right"></i>
+      <div class="text">NEXT CHAPTER</div>
+    </div>
+    <div class="scroll">
+      <div class="pre"><i class="fas fa-caret-left"></i></div>
+      <div class="inner-scroll" :style="{width: `${totalLength}px`, left: `${position}px`}">
+        <div class="scroll-box" :style="{left: `${scrollpostion}px`}"></div>
       </div>
-      <div class="next-button" @click="changeChapter(1)">
-        <i class="fas fa-angle-double-right"></i>
-        <div class="text">NEXT CHAPTER</div>
-      </div>
+      <div class="next"><i class="fas fa-caret-right"></i></div>
+    </div>
   </div>
 </template>
 
@@ -30,13 +37,22 @@ export default {
   name: 'ReadingTop',
   data () {
     return {
-      msg: 'Chapters',
+      position: 0,
+      scrollpostion: 0
     }
   },
   computed: {
     ...mapState('chapters',['chapters', 'nowChapter', 'nowPage']),
     localChapterPages() {
       return this.chapters[this.nowChapter].pages;
+    },
+    centerPositionNum() {
+      return document.querySelector('.slider').offsetWidth;
+    },
+    totalLength() {
+      let pageNum = this.localChapterPages.length;
+
+      return pageNum * 80 + (pageNum - 1) * 20;
     }
   },
   methods: {
@@ -52,7 +68,17 @@ export default {
       nowChapter = nowChapter + num;
       this.CHANGE_CHAPTER({index: nowChapter});
     }
-    
+  },
+  watch: {
+    nowPage() {
+      let totals = this.localChapterPages.length;
+      let centerNum = Math.floor(this.centerPositionNum / 2 / 80) - 1;
+      let floorNum = centerNum - 1;
+      if(this.nowPage > floorNum && this.nowPage + 2 < totals) {
+        this.position = (this.nowPage - centerNum) * -100;
+      }
+      this.scrollpostion = this.nowPage * 100;
+    }
   }
 }
 </script>
